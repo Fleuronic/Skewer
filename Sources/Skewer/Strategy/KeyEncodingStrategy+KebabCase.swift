9807cac32322a96ba3ff54ef9dee52cd30fd8b4a
Jordan Kay <jordan@fleuronic.com>
2020-04-29 18:50:23 -0700

    Update .swiftformat

--- Sources/Skewer/Strategy/KeyEncodingStrategy+KebabCase.swift
+++ Sources/Skewer/Strategy/KeyEncodingStrategy+KebabCase.swift
@@ -19,11 +19,11 @@ private extension String {
 
 		var wordStart = startIndex
 		var wordRanges: [Range<Index>] = []
-		var searchRange = index(after: wordStart) ..< endIndex
+		var searchRange = index(after: wordStart)..<endIndex
 
 		while let uppercaseRange = rangeOfCharacter(from: .uppercaseLetters, range: searchRange) {
-			wordRanges.append(wordStart ..< uppercaseRange.lowerBound)
-			searchRange = uppercaseRange.lowerBound ..< searchRange.upperBound
+			wordRanges.append(wordStart..<uppercaseRange.lowerBound)
+			searchRange = uppercaseRange.lowerBound..<searchRange.upperBound
 
 			guard let lowercaseRange = rangeOfCharacter(from: .lowercaseLetters, range: searchRange) else {
 				wordStart = searchRange.lowerBound
@@ -31,17 +31,17 @@ private extension String {
 			}
 
 			let nextCharacterAfterCapital = index(after: uppercaseRange.lowerBound)
-			searchRange = lowercaseRange.upperBound ..< searchRange.upperBound
+			searchRange = lowercaseRange.upperBound..<searchRange.upperBound
 
 			if lowercaseRange.lowerBound == nextCharacterAfterCapital {
 				wordStart = uppercaseRange.lowerBound
 			} else {
 				let beforeLowerIndex = index(before: lowercaseRange.lowerBound)
-				wordRanges.append(uppercaseRange.lowerBound ..< beforeLowerIndex)
+				wordRanges.append(uppercaseRange.lowerBound..<beforeLowerIndex)
 				wordStart = beforeLowerIndex
 			}
 		}
-		wordRanges.append(wordStart ..< searchRange.upperBound)
+		wordRanges.append(wordStart..<searchRange.upperBound)
 
 		let words = wordRanges.map { self[$0] }
 		let lowercaseWords = words.map { $0.lowercased() }
