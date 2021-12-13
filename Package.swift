edf4693b7a7c365f9897c80dcf54ebd5187f873d
Jordan Kay <jordan@fleuronics-mbp.myfiosgateway.com>
2021-12-13 17:51:53 -0500

    Support down to Swift 5.1

--- Package.swift
+++ Package.swift
@@ -1,10 +1,17 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1
 // Copyright © Fleuronic LLC. All rights reserved.
 
 import PackageDescription
 
+#if swift(>=5.5)
 let package = Package(
 	name: "Skewer",
+	platforms: [
+		.iOS(.v13),
+		.macOS(.v10_15),
+		.watchOS(.v6),
+		.tvOS(.v13)
+	],
 	products: [
 		.library(
 			name: "Skewer",
@@ -22,3 +29,26 @@ let package = Package(
 		)
 	]
 )
+#else
+let package = Package(
+	name: "Skewer",
+	platforms: [
+		.iOS(.v13),
+		.macOS(.v10_15),
+		.watchOS(.v6),
+		.tvOS(.v13)
+	],
+	products: [
+		.library(
+			name: "Skewer",
+			targets: ["Skewer"]
+		)
+	],
+	targets: [
+		.target(
+			name: "Skewer",
+			dependencies: []
+		)
+	]
+)
+#endif
