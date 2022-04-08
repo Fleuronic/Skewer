05ade7f6e556ce1935c62f4f7b1fa43d0e29b957
Jordan Kay <jordan@fleuronic.com>
2022-04-08 11:11:27 -0400

    Simplify Package.swift

--- Package.swift
+++ Package.swift
@@ -1,54 +1,31 @@
-// swift-tools-version:5.1
-// Copyright © Fleuronic LLC. All rights reserved.
+// swift-tools-version:5.3
 
 import PackageDescription
 
-#if swift(>=5.5)
-let package = Package(
-	name: "Skewer",
-	platforms: [
-		.iOS(.v13),
-		.macOS(.v10_15),
-		.watchOS(.v6),
-		.tvOS(.v13)
-	],
-	products: [
-		.library(
-			name: "Skewer",
-			targets: ["Skewer"]
-		)
-	],
-	targets: [
-		.target(
-			name: "Skewer",
-			dependencies: []
-		),
-		.testTarget(
-			name: "SkewerTests",
-			dependencies: ["Skewer"]
-		)
-	]
-)
-#else
-let package = Package(
-	name: "Skewer",
-	platforms: [
-		.iOS(.v13),
-		.macOS(.v10_15),
-		.watchOS(.v6),
-		.tvOS(.v13)
-	],
-	products: [
-		.library(
-			name: "Skewer",
-			targets: ["Skewer"]
-		)
-	],
-	targets: [
-		.target(
-			name: "Skewer",
-			dependencies: []
-		)
-	]
+var targets: [Target] = [.target(name: "Skewer")]
+
+#if swift(>=5.4)
+targets.append(
+	.testTarget(
+		name: "SkewerTests",
+		dependencies: ["Skewer"]
+	)
 )
 #endif
+
+let package = Package(
+	name: "Skewer",
+	platforms: [
+		.iOS(.v13),
+		.macOS(.v10_15),
+		.watchOS(.v6),
+		.tvOS(.v13)
+	],
+	products: [
+		.library(
+			name: "Skewer",
+			targets: ["Skewer"]
+		)
+	],
+	targets: targets
+)
